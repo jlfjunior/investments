8ee331fb24d7487e309ad8dadaf2598ea34cbea1
Jose Junior <jlfjunior8@gmail.com>
2023-08-31 08:26:36 -0300

    feat: add operations domain

---
+++ src/Investments.Operations.Domain/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using Investments.Core.DomainObjects;
+
+namespace Investments.Operations.Domain.Entities;
+
+public class Order : Entity, IAggregateRoot
+{
+    public Guid ProductId { get; private set; }
+    public decimal Amount { get; private set; }
+    public decimal Quantity { get; private set; }
+    public DateTime OperatedAt { get; private set; }
+
+    protected Order() { }
+
+    public Order(Guid productId, decimal amount, decimal quantity, DateTime operatedAt)
+    {
+        ProductId = productId;
+        Amount = amount;
+        Quantity = quantity;
+        OperatedAt = operatedAt;
+    }
+
+    protected override void IsValid()
+    {
+    }
+}
