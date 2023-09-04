41c4a3f6fbbfdb73a6248191ab7256374f4aa503
Jose Junior <jlfjunior8@gmail.com>
2023-09-04 08:39:00 -0300

    feat: implement infrastructure data to operations domain

---
+++ src/Investments.Operations.Infrastructure.Data/OperationsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Investments.Operations.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Investments.Operations.Infrastructure.Data;
+
+public class OperationsDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Operation> Operations { get; set; }
+    
+    public OperationsDbContext(DbContextOptions<OperationsDbContext> options) 
+        : base(options)
+    {
+    }
+}
